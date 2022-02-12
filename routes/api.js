b7cc169a7aeb8d7422402eee18efe01c05584c2f
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2022-02-12 13:38:18 +0000

    Get all recipes endpoint without filtering

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,21 @@
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
+const { readFile } = require("fs/promises");
+// const { getRecipes } = require("../controllers/recipes");
 
-apiRouter.get('/', (_, res) => {
-  res.json({ message: 'ok' });
+apiRouter.get("/", (_, res) => {
+  res.json({ message: "ok" });
+});
+
+// Endpoint to Get all recipes
+
+apiRouter.get(`/recipes`, (req, res) => {
+  readFile("./data/data.json", "utf8")
+    .then((data) => {
+      res.status(200).send({ recipes: JSON.parse(data) });
+    })
+    .catch(() => {
+      res.status(404).send({ error: "No recipes found" });
+    });
 });
 
 module.exports = apiRouter;
--- test/api.test.js
+++ test/api.test.js
@@ -1,9 +1,22 @@
-const supertest = require('supertest');
-const server = require('../server');
+const supertest = require("supertest");
+const server = require("../server");
 
 const request = supertest(server);
 
-test('/api', async () => {
-  const { body } = await request.get('/api').expect(200);
-  expect(body.message).toBe('ok');
+test("/api", async () => {
+  const { body } = await request.get("/api").expect(200);
+  expect(body.message).toBe("ok");
+});
+test("/api/recipes", () => {
+  //const { body } = await request;
+  return request.get("/api/recipes").expect(200);
+  // .then((res) => {
+  //   console.log(res, "<======");
+  //   expect(res).toContainKeys([
+  //     "id",
+  //     "imageUrl",
+  //     "instructions",
+  //     "ingredients",
+  //   ]);
+  // });
 });
